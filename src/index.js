53c331e66ae59c0d47889b299f969793e358c713
Ryan <ryanying1102@gmail.com>
2024-07-08 00:43:52 -0400

    added newTask function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import './style.css';
 
-let tasks = [];
+let tasks = []
+
+function task (title, description, duedate, prio) {
+    this.title = title;
+    this.description = description;
+    this.duedate = duedate;
+    this.prio = prio;
+}
+
+const addNewTask = (() => {
+    const logNewTask = (() => {
+        const taskTitle = document.getElementById('newtitle').value;
+        const taskDescription = document.getElementById('newdescription').value;
+        const taskDueDate = document.getElementById('newduedate');
+        const taskPrio = document.getElementById('newprio');
+        
+        const newTask = new task (taskTitle, taskDescription, taskDueDate, taskPrio);
+        tasks.push(newTask);
+    });
+
+    return {
+        logNewTask
+    };
+})();
+
+
+const updateScreen = (() => {
+
+})();
 
 const btnAddTask = document.getElementById('addTask');
 const taskModal = document.getElementById('taskModal');
@@ -10,6 +38,7 @@ btnAddTask.addEventListener('click', () => {
     taskModal.showModal();
 });
 
-const addNewTask = (() => {
-    
+const testBtn = document.getElementById('test');
+testBtn.addEventListener('click', () => {
+    addNewTask.logNewTask();
 });

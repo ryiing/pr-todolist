0c410a97ce850a35f888e5ac19f24dbe33e458af
ryiing <ryanying1102@gmail.com>
2024-07-09 20:03:59 -0400

    added screen controller

--- src/index.js
+++ src/index.js
@@ -11,34 +11,87 @@ function task (title, description, duedate, prio) {
 
 const addNewTask = (() => {
     const logNewTask = (() => {
-        const taskTitle = document.getElementById('newtitle').value;
-        const taskDescription = document.getElementById('newdescription').value;
-        const taskDueDate = document.getElementById('newduedate');
-        const taskPrio = document.getElementById('newprio');
-        
-        const newTask = new task (taskTitle, taskDescription, taskDueDate, taskPrio);
-        tasks.push(newTask);
-    });
+        const newtaskTitleElement = document.getElementById('newtitle');
+        const newtaskDescriptionElement = document.getElementById('newdescription');
+        const newtaskDueDateElement = document.getElementById('newduedate');
+        const newtaskPrioElement = document.getElementById('newprio');
+    
+        const newtaskTitle = newtaskTitleElement.value;
+        const newtaskDescription = newtaskDescriptionElement.value;
+        const newtaskDueDate = newtaskDueDateElement.value;
+        const newtaskPrio = newtaskPrioElement.value;
 
+        const newTask = new task (newtaskTitle, newtaskDescription, newtaskDueDate, newtaskPrio);
+        tasks.push(newTask);
+
+        newtaskTitleElement.value = '';
+        newtaskDescriptionElement.value = '';
+        newtaskDueDateElement.value = '';
+        newtaskPrioElement.value = '';
+    });
+ 
     return {
         logNewTask
     };
 })();
 
 
-const updateScreen = (() => {
+const screenController = (() => {
+
+    const createTaskDiv = ((index) => {
+        const main = document.getElementById('main'); 
+
+        const taskDiv = document.createElement('div');
+        taskDiv.classList.add('taskContainer');
+    
+        // const taskTitle = document.createElement('div');
+        const taskDescription = document.createElement('div');
+        const taskDueDate = document.createElement('div');
+        const taskPrio = document.createElement('div');
+
+        // taskTitle.innerHTML = 'test';
+        // taskDiv.appendChild(taskTitle);
+        
+        taskDiv.appendChild(Object.assign(document.createElement('div'),{innerHTML: `Title: ` + tasks[index].title}));
+        taskDiv.appendChild(Object.assign(document.createElement('div'),{innerHTML: `Description: ` + tasks[index].description}));
+        taskDiv.appendChild(Object.assign(document.createElement('div'),{innerHTML: `DueDate: ` + tasks[index].duedate}));
+        taskDiv.appendChild(Object.assign(document.createElement('div'),{innerHTML: `Priority: ` + tasks[index].prio}));
+        main.appendChild(taskDiv);
+    });
+
+    const updateScreen = (() => {
+        for (let i=0; i < tasks.length; i++) {
+            createTaskDiv(i);
+        }
+    });
+
+    return {
+        updateScreen
+    };
 
 })();
 
-const btnAddTask = document.getElementById('addTask');
-const taskModal = document.getElementById('taskModal');
+const main = (() => {
+    const btnAddTask = document.getElementById('addTask');
+    const taskModal = document.getElementById('taskModal');
+    const formAdd = document.getElementById('submit')
+    
+    btnAddTask.addEventListener('click', () => {
+        console.log('test');
+        taskModal.showModal();
+    });
+    
+    const testBtn = document.getElementById('close');
+    testBtn.addEventListener('click', () => {
+        taskModal.close();
+    });
 
-btnAddTask.addEventListener('click', () => {
-    console.log('test');
-    taskModal.showModal();
+    formAdd.addEventListener('click', () => {
+        addNewTask.logNewTask();
+        taskModal.close();
+        screenController.updateScreen();
+    });
 });
 
-const testBtn = document.getElementById('test');
-testBtn.addEventListener('click', () => {
-    addNewTask.logNewTask();
-});
+main();
+
